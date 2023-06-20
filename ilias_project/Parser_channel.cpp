4003b3877b1919821f23407ee6827ca860c32c0b
Ilyas Lkridah <ilkridah@e2r9p1.1337.ma>
2023-06-20 19:18:22 +0100

    kick

--- ilias_project/Parser_channel.cpp
+++ ilias_project/Parser_channel.cpp
@@ -111,27 +111,33 @@ std::vector<std::pair<std::string, std::string> > Parser::parse_kick(
     std::string::iterator it,
     std::string::iterator end) {
     std::vector<std::pair<std::string, std::string> > chan_key;
-    std::vector<std::string> chan;
     std::vector<std::string> args = parse_all(it, end, "KICK");
 
     if (args.size() < 2)
         throw IRCException::ERR_NEEDMOREPARAMS("KICK");
+
+    std::vector<std::string> chan;
     if (args[0].find(',') != std::string::npos)
         chan = spliter(args[0], ',');
     else
         chan.push_back(args[0]);
-    for (size_t i = 0; i < chan.size(); i++) {
-        if (chan[i][0] != '#')
-            throw IRCException::ERR_NOSUCHCHANNEL(chan[i]);
-        chan_key.push_back(std::make_pair(chan[i], ""));
-    }
+
     std::vector<std::string> key;
     if (args[1].find(',') != std::string::npos)
         key = spliter(args[1], ',');
     else
         key.push_back(args[1]);
-    for (size_t i = 0; i < key.size() && key.size() <= chan.size(); i++)
-        chan_key[i].second = key[i];
+
+    std::vector<std::string>::iterator chanIter = chan.begin();
+    std::vector<std::string>::iterator keyIter;
+    for (; chanIter != chan.end(); chanIter++) {
+        keyIter = key.begin();
+        if ((*chanIter)[0] != '#')
+            throw IRCException::ERR_NOSUCHCHANNEL(*chanIter);
+        for(;keyIter != key.end(); keyIter++)
+            chan_key.push_back(std::make_pair(*chanIter, *keyIter));
+    }
+
     return chan_key;
 }
 
@@ -153,7 +159,7 @@ std::vector<std::string> Parser::parse_invite(std::string::iterator it,
     return args;
 }
 std::vector<std::string> Parser::parse_api(std::string::iterator it,
-                                              std::string::iterator end) {
+                                           std::string::iterator end) {
     if (it == end)
         throw IRCException::ERR_NEEDMOREPARAMS("/WEATHER/API");
     std::vector<std::string> args;
